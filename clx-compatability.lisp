1b2b22fae5cc7d799c0504a694f530b632930ddf
Thibault Langlois <tl@di.fc.ul.pt>
2022-07-03 19:06:27 +0100

    clx-compatibility gives compilation errors  with recent sbcl

--- clx-compatability.lisp
+++ clx-compatability.lisp
@@ -2,16 +2,21 @@
 
 (in-package :xlib)
 
+#+nil
 (defun pixmap-p (object)
   (typep object 'pixmap))
 
+#+nil
 (defun image-z-p (object)
   (typep object 'image-z))
 
+#+nil
 (export 'xlib::image-z-p :xlib)
 
+#+nil
 (defun pixmap-plist (pixmap)
   (xlib:drawable-plist pixmap))
 
+#+nil
 (defun (setf pixmap-plist) (value window)
   (setf (drawable-plist window) value))
